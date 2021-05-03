e6c0f14176f38d8308ceddd1df42fa4fa829fda0
Dmitry <dimk00z@gmail.com>
2021-05-03 20:37:05 +0300

    Update 3_simple_recursion.py

--- chapter 3/3_simple_recursion.py
+++ chapter 3/3_simple_recursion.py
@@ -4,15 +4,25 @@ def infinity_countdown(value):
 
 
 def normal_countdown(value):
-    print(value)
+    # print(value)
     if value == 0:
         return
     normal_countdown(value-1)
 
 
+def simple_factorial(value):
+    return 1 if value == 1 else value*simple_factorial(value-1)
+
+
+def reverse_string(string):
+    return '' if string == '' else reverse_string(string[1:])+string[0]
+
+
 def main():
     # infinity_countdown(10)
-    normal_countdown(10)
+    normal_countdown(5)
+    print(simple_factorial(3))
+    print(reverse_string('cat'))
 
 
 if __name__ == '__main__':
