ac82d19cd5f310d11ee73f1435002980a89d1311
Dmitry <dimk00z@gmail.com>
2021-05-03 21:02:20 +0300

    Update 3_simple_recursion.py

--- chapter 3/3_simple_recursion.py
+++ chapter 3/3_simple_recursion.py
@@ -4,7 +4,6 @@ def infinity_countdown(value):
 
 
 def normal_countdown(value):
-    # print(value)
     if value == 0:
         return
     normal_countdown(value-1)
@@ -18,11 +17,17 @@ def reverse_string(string):
     return '' if string == '' else reverse_string(string[1:])+string[0]
 
 
+def recursive_sum(array):
+    print(array)
+    return array[0] if len(array) == 1 else array[0] + recursive_sum(array[1:])
+
+
 def main():
     # infinity_countdown(10)
     normal_countdown(5)
     print(simple_factorial(3))
     print(reverse_string('cat'))
+    print(recursive_sum([1, 2, 3, 4, 5, 6]))
 
 
 if __name__ == '__main__':
