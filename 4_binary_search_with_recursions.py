b74655d07549a0d37215ccc6dcbbed0d86002e76
Dmitry <dimk00z@gmail.com>
2021-05-05 14:31:04 +0300

    rename

--- 4_binary_search_with_recursions.py
+++ 4_binary_search_with_recursions.py
@@ -3,17 +3,19 @@ from utils import create_ordered_list
 
 def binary_search_with_recursion(
         ordered_list, required_item, left, right):
-    if right >= left:
-        middle = (left+right)//2
-        # print(middle, ordered_list[middle])
-        if ordered_list[middle] == required_item:
-            return middle
-        elif ordered_list[middle] > required_item:
-            return binary_search_with_recursion(
-                ordered_list, required_item, left, middle-1)
-        else:
-            return binary_search_with_recursion(
-                ordered_list, required_item, middle+1, right)
+    middle = (left+right)//2
+
+    # basic case
+    if ordered_list[middle] == required_item:
+        return middle
+
+    # recursive case
+    if ordered_list[middle] > required_item:
+        return binary_search_with_recursion(
+            ordered_list, required_item, left, middle-1)
+    else:
+        return binary_search_with_recursion(
+            ordered_list, required_item, middle+1, right)
 
 
 def main():
